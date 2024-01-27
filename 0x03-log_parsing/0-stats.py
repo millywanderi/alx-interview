3e52bfd806661c944862b08ad4e2ea649ebf07e7
millywanderi <wanderinyambura5@gmail.com>
2024-01-27 12:00:18 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,8 @@
 import sys
 
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
 
 total_size = 0
 counter = 0
@@ -16,21 +16,21 @@ try:
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
+            if code in status_code.keys():
+                status_code[code] += 1
             total_size += size
             counter += 1
         if counter == 10:
             counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
+            print(f"File size: {total_size}")
+            for key, value in sorted(status_code.items()):
                 if value != 0:
-                    print('{}: {}'.format(key, value))
-except Exception as err:
+                    print(f"{key}: {value}")
+except Exception:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
+    print("File size: {total_size}")
+    for key, value in sorted(status_code.items()):
         if value != 0:
-            print('{}: {}'.format(key, value))
+            print(f"{key}: {value}")
