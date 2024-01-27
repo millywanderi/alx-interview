a5482a502e691a99fe6befeae4877982bb4261ff
Millicent Wanderi <122854714+millywanderi@users.noreply.github.com>
2024-01-27 20:38:42 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,10 @@
 """Printing status"""
 import sys
 
-
-status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
-
+# Initializes a dict named cache with stat codes as keys and
+# their counts as values, all swt to 0 initially
 total_size = 0
 counter = 0
 
@@ -15,21 +15,22 @@ try:
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in status_code.keys():
-                status_code[code] += 1
+            if code in cache.keys():
+                cache[code] += 1
             total_size += size
             counter += 1
         if counter == 10:
             counter = 0
-            print(f"File size: {total_size}")
-            for key, value in sorted(status_code.items()):
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
                 if value != 0:
-                    print(f"{key}: {value}")
-except Exception:
+                    print('{}: {}'.format(key, value))
+except Exception as err:
     pass
 
 finally:
-    print("File size: {total_size}")
-    for key, value in sorted(status_code.items()):
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
         if value != 0:
-            print(f"{key}: {value}")
+            print('{}: {}'.format(key, value))
+
